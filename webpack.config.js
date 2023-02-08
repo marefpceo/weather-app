10e8e4cee8ab42591e03a00b8c758c07bdf98f23
marefpceo <marefpceo@gmail.com>
2023-02-08 08:59:23 -0800

    Folder structure created and webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    clean: {
+      keep: /index.html/,
+    },
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: './assets/[name][ext][query]',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
